8f8d128d066b34d9861a90641e3e6d3ebdfb9914
Zeus2604 <83212383+Zeus2604@users.noreply.github.com>
2026-07-05 11:14:39 -0500

    Update page.tsx
    Simplify page.tsx for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-black text-white">
-      <div className="max-w-7xl mx-auto px-6 py-8">
-        <h1 className="text-7xl font-bold text-center leading-none tracking-tighter">
-          EL 10 DE LA CUADRA
-        </h1>
-      </div>
-    </main>
-  );
+  return <h1>Hola</h1>;
 }
