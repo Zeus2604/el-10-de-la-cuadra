d5a7afb50607df067642bfd0ee24aae459326329
Zeus2604 <83212383+Zeus2604@users.noreply.github.com>
2026-07-05 11:19:46 -0500

    Update page.tsx
    Add basic homepage design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Hola</h1>;
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1 className="text-6xl font-bold text-center py-20 text-yellow-400">
+        EL 10 DE LA CUADRA
+      </h1>
+    </main>
+  );
 }
